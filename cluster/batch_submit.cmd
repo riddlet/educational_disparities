d3a15424c99b7ed7f372a2c2c9e1ca3dc833eb2c
Travis Riddle <travis@nat-oitwireless-inside-vapornet100-a-21812.Princeton.EDU>
2017-11-08 10:21:03 -0500

    batch submit scripts

--- cluster/batch_submit.cmd
+++ cluster/batch_submit.cmd
@@ -1,14 +1,8 @@
 #!/bin/bash
-# serial job using 1 node and 1 processor,
-# and runs for 1 minute (max).
 #SBATCH -N 1   # node count
 #SBATCH --ntasks-per-node=4  # core count
-#SBATCH -t 4-00:00:00 
+#SBATCH -t 3-00:00:00 
 #SBATCH --mem-per-cpu=5000
-# sends mail when process begins, and
-# when it ends. Make sure you define your email
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
-#SBATCH --mail-user=triddle@princeton.edu
+
 
 Rscript --no-save --no-restore --verbose $1 > $1.Rout
