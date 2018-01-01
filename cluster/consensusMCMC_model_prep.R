138341975e85ba65a6abcb0afe10d5a6cea5d3ce
Travis Riddle <travis@Traviss-MacBook-Air.local>
2018-01-01 11:53:55 -0500

    adjust to standardize variables before splitting

--- cluster/consensusMCMC_model_prep.R
+++ cluster/consensusMCMC_model_prep.R
@@ -26,15 +26,13 @@ options(mc.cores = parallel::detectCores())
 
 df <- read.csv('/home/triddle/educational_disparities/cluster/data/county_grouping.csv')
 
-df %>% filter(grouping=="
-
-string2 <-  ") %>% filter(metric=='"
-string3 <- "') %>% select(county_id, bias:weighted_warmth, total_pop, unemp_rate:b.w.ratio) %>% distinct() %>% mutate_at(vars(-county_id), scale) -> scaled_county_level
+df %>%  filter(metric=='"
+string2 <- "') %>% select(county_id, bias:weighted_warmth, total_pop, unemp_rate:b.w.ratio) %>% distinct() %>% mutate_at(vars(-county_id), scale) -> scaled_county_level
 
 df %>% filter(grouping=="
 
-string4 <- ") %>% filter(metric=='"
-string5 <- "') %>% select(county_id, COMBOKEY:metric) %>% left_join(scaled_county_level) -> mod.dat
+string3 <- ") %>% filter(metric=='"
+string4 <- "') %>% select(county_id, COMBOKEY:metric) %>% left_join(scaled_county_level) -> mod.dat
 
 m <- stan_glmer(cbind(number, total_number-number) ~ group + bias + warmth + 
 group:bias + group:warmth + total_pop + unemp_rate + 
@@ -43,11 +41,11 @@ black_prop + b.w.ratio +
 (group|county_id), data=mod.dat, prior = normal(0,5), 
 prior_intercept = normal(0,5), family=binomial, adapt_delta=.99)
 
-save(m, file='/tigress/triddle/educational_disparities/metrics_raw/"
+save(m, file='/scratch/network/triddle/educational_disparities/metrics_raw/"
 
 for(i in rownames(table(dfs$metric))){
   for (j in 1:10){
-    fs <- paste(string1, j, string2, i, string3, j, string4, i, string5, i, "/m", j, ".rdata')", sep='')
+    fs <- paste(string1, i, string2, j, string3, i, string4, i, "/m", j, ".rdata')", sep='')
     fileConn <- file(paste("model_scripts/metrics_raw/",i,"/m", j, ".R", sep=''))
     writeLines(fs, fileConn)
     close(fileConn)
