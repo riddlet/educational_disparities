d5894ac8d4cb01e49b64ac087d844bce4ff34b58
Travis Riddle <travis@Traviss-MacBook-Air.local>
2018-01-14 17:07:00 -0500

    teacher explicit diffs

--- cluster/consensusMCMC_model_prep.R
+++ cluster/consensusMCMC_model_prep.R
@@ -4,6 +4,7 @@ dfs <- read.csv('output/full_model_data.csv')
 dfss <- read.csv('output/selected_model_data_ucla_excl.csv')
 dfss_diff <- read.csv('output/selected_model_data_ucla_excl_exp_diff.csv')
 dft <- read.csv('output/teacher_model_data.csv')
+dft_diff <- read.csv('output/teacher_model_data_expdiff.csv')
 
 ######## write consensus MCMC files
 dfs %>% 
@@ -18,7 +19,7 @@ counties %>%
   mutate(grouping=sample(1:11, n(), replace=T)) -> county_assignments
 
 teacher_counties %>%
-  mutate(grouping=sample(1:9, n(), replace=T)) -> teacher_county_assignments
+  mutate(grouping=sample(1:11, n(), replace=T)) -> teacher_county_assignments
 
 dfs %>% 
   left_join(county_assignments) -> county_groups #append the grouping information
@@ -28,12 +29,14 @@ dfss_diff %>%
   left_join(county_assignments) -> subcounty_groups_diff
 dft %>%
   left_join(teacher_county_assignments) -> teacher_groups
+dft_diff %>%
+  left_join(teacher_county_assignments) -> teacher_groups_diff
 
 write.csv(county_groups, file='data/county_grouping.csv', row.names = F)
 write.csv(subcounty_groups, file='data/subcounty_grouping_ucla_excl.csv', row.names=F)
 write.csv(subcounty_groups_diff, file='data/subcounty_grouping_diff.csv', row.names=F)
 write.csv(teacher_groups, file='data/county_teacher_grouping.csv', row.names=F)
-
+write.csv(teacher_groups_diff, file='data/county_teacher_grouping_expdiff.csv', row.names=F)
 
 ######################## write raw files #############
 
@@ -145,7 +148,7 @@ save(m, file='/tigress/triddle/educational_disparities/teacher_metrics_della/"
 
 
 for(i in rownames(table(dft$metric))){
-  for (j in 1:9){
+  for (j in 1:11){
     fs <- paste(string1, i, string2, j, string3, i, string4, i, "/m", j, ".rdata')", sep='')
     fileConn <- file(paste("model_scripts/teacher_metrics/",i,"/m", j, ".R", sep=''))
     writeLines(fs, fileConn)
@@ -230,6 +233,44 @@ for(i in rownames(table(dfss$metric))){
   for (j in 1:11){
     fs <- paste(string1, i, string2, j, string3, i, string4, i, "/m", j, ".rdata')", sep='')
     fileConn <- file(paste("model_scripts/mw_uclaexcl_diff//",i,"/m", j, ".R", sep=''))
+    writeLines(fs, fileConn)
+    close(fileConn)
+  }
+}
+
+######################## write teacher files for explicit diffs #############
+string1 <- "library(dplyr)
+library(tidyr)
+library(forcats)
+library(ggplot2)
+library(rstanarm)
+
+options(mc.cores = parallel::detectCores())
+
+df <- read.csv('/home/triddle/educational_disparities/cluster/data/county_teacher_grouping_expdiff.csv')
+
+df %>% filter(metric=='"
+
+string2 <- "') %>% select(county_id, county_bias:county_warmth, total_pop, unemp_rate:b.w.ratio) %>% distinct() %>% mutate_at(vars(-county_id), scale) -> scaled_county_level
+
+df %>% filter(grouping=="
+
+string3 <- ") %>% filter(metric=='"
+string4 <- "') %>% select(COMBOKEY:metric, county_id) %>% left_join(scaled_county_level) -> mod.dat
+
+m <- stan_glmer(cbind(number, total_number-number) ~ group + county_bias + 
+county_warmth + group:county_bias + group:county_warmth + total_pop + 
+unemp_rate + med_income + poverty_rate + col_grads + white_prop + black_prop + b.w.ratio + 
+(group|county_id), data=mod.dat, prior = normal(0,5), 
+prior_intercept = normal(0,5), family=binomial, adapt_delta=.99)
+
+save(m, file='/tigress/triddle/educational_disparities/teacher_metrics_expdiff/"
+
+
+for(i in rownames(table(dft$metric))){
+  for (j in 1:11){
+    fs <- paste(string1, i, string2, j, string3, i, string4, i, "/m", j, ".rdata')", sep='')
+    fileConn <- file(paste("model_scripts/teacher_metrics_expdiff/",i,"/m", j, ".R", sep=''))
     writeLines(fs, fileConn)
     close(fileConn)
   }
