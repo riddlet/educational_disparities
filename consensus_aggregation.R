0d84498f1d34a4cec3e49426856aee7f2c99d159
Travis Riddle <travis@nat-oitwireless-inside-vapornet100-d-19882.Princeton.EDU>
2018-08-16 14:41:04 -0400

    aggregate for SES model

--- consensus_aggregation.R
+++ consensus_aggregation.R
@@ -5,6 +5,410 @@ library(ggplot2)
 #warmth: 0=very cold, 10=very warm
 #bias: higher scores = more bias
 
+base_path <- '/Users/travis/Documents/gits/educational_disparities/cluster/output/mw_2013uclaexcl_pca/'
+plot.dat <- data.frame(group = NA, weighted_bias = NA, est = NA, lower = NA, upper=NA, metric = NA)
+plot.dat2 <- data.frame(weighted_bias = NA, est = NA, lower=NA, upper=NA, metric=NA)
+plot.dat3 <- data.frame(est=NA, lower=NA, upper=NA, metric=NA)
+
+# weighted bias w/exclusions & explicit difference
+metrics <- list.files(base_path)
+for (i in metrics){
+  pth <- paste(base_path, i, '/', sep='')
+  
+  files <- list.files(pth)
+  j <- 1
+  posterior_combo <- array(0, dim=c(24,4000,length(files)))
+  raw_rate <- data.frame(county_id=NA, nschools=NA, weighted_bias=NA, group=NA, nincidents=NA, nstudents=NA, rate=NA)
+  if (length(files>1)){
+    for (k in files){
+      #print(i)
+      load(paste(pth,k,sep=''))
+      df <- as.matrix(m)
+      m$data %>% 
+        select(county_id, COMBOKEY) %>% 
+        distinct() %>% 
+        group_by(county_id) %>% 
+        summarise(nschools=n()) -> n_schools
+      m$data %>%
+        group_by(county_id, group) %>%
+        summarise(nincidents=sum(number), nstudents=sum(total_number)) %>%
+        mutate(rate=nincidents/nstudents) -> rate
+      n_schools %>%
+        left_join(m$data[, c('county_id', 'weighted_bias')]) %>%
+        distinct() %>%
+        left_join(rate) %>%
+        rbind(raw_rate) %>%
+        filter(!is.na(county_id)) -> raw_rate
+      posterior_combo[,,j] <- t(df[,c(1:24)])
+      j <- j+1
+    }
+    p_cons <- parallelMCMCcombine::consensusMCcov(posterior_combo, shuff = T)
+    
+    for (p in 1:24){
+      m$stanfit@sim$samples[[1]][[p]] <- p_cons[p,1:1000]
+    }
+    for (p in 1:24){
+      m$stanfit@sim$samples[[2]][[p]] <- p_cons[p,1001:2000]
+    }
+    for (p in 1:24){
+      m$stanfit@sim$samples[[3]][[p]] <- p_cons[p,2001:3000]
+    }
+    for (p in 1:24){
+      m$stanfit@sim$samples[[4]][[p]] <- p_cons[p,3001:4000]
+    }
+  }else{
+    load(paste(pth,files,sep=''))
+    df <- as.matrix(m)
+    m$data %>% 
+      select(county_id, COMBOKEY) %>% 
+      distinct() %>% 
+      group_by(county_id) %>% 
+      summarise(nschools=n()) -> n_schools
+    # m$data %>% 
+    #   group_by(county_id, group) %>% 
+    #   summarise(nincidents=sum(number), nstudents=sum(total_number)) %>% 
+    #   mutate(rate=nincidents/nstudents) -> rate
+    # n_schools %>% 
+    #   left_join(m$data[, c('county_id', 'weighted_bias')]) %>%
+    #   distinct() %>%
+    #   left_join(rate) %>%
+    #   rbind(raw_rate) %>%
+    #   filter(!is.na(county_id)) -> raw_rate
+    
+  }
+  
+  newdat <- expand.grid(group = rownames(table(m$data$group)),
+                        total_pop=0,
+                        ses=0,
+                        unemp_rate=0,
+                        med_income=0,
+                        poverty_rate=0,
+                        col_grads=0,
+                        white_prop=0,
+                        black_prop=0,
+                        b.w.ratio=0,
+                        mobility=0,
+                        crime_rate=0,
+                        dissim=0,
+                        density=0,
+                        weighted_bias = seq(-2.25,2.25,.25),
+                        weighted_explicit_diff = 0,
+                        number=0,
+                        total_number=1)
+  y_hat <- rstanarm::posterior_linpred(m, newdata = newdat, re.form=~0, transform=T)
+  
+  cbind(newdat, t(y_hat)) %>% 
+    mutate(index=row_number()) %>%
+    gather(sample, value, `1`:`4000`) -> posterior_distribution
+  
+  
+  posterior_distribution %>%
+    select(group, weighted_bias, sample, value, index) %>%
+    group_by(group, weighted_bias) %>%
+    summarise(est = mean(value),
+              lower = quantile(value, .025),
+              upper = quantile(value, .975)) %>%
+    ungroup() %>%
+    mutate(metric=i) %>%
+    rbind(plot.dat) %>% 
+    filter(!is.na(group)) -> plot.dat
+  
+  ggplot(plot.dat, aes(x=weighted_bias, y=est, group=group)) +
+    geom_line(aes(color=group)) +
+    geom_ribbon(aes(ymin=lower, ymax=upper), alpha=.15) +
+    theme_classic() +
+    #ylim(0,quantile(raw_rate$rate, .9))
+    facet_wrap(~metric, scales = 'free') -> plot
+  
+  plot_path <- '/Users/travis/Documents/gits/educational_disparities/figs/individual_models/mw_2013uclaexcl_pca/bias.jpeg'
+  
+  ggsave(filename=plot_path, plot)
+  
+  
+  posterior_distribution %>%
+    select(group, weighted_bias, sample, value) %>%
+    spread(group, value) %>%
+    mutate(ratio = black/white) %>%
+    select(weighted_bias, sample, ratio) %>%
+    group_by(weighted_bias) %>%
+    summarise(est = mean(ratio),
+              lower = quantile(ratio, .025),
+              upper = quantile(ratio, .975)) %>%
+    ungroup() %>%
+    mutate(metric=i) %>%
+    rbind(plot.dat2) %>% 
+    filter(!is.na(weighted_bias)) -> plot.dat2
+  
+  raw_rate %>%
+    select(county_id, nschools, weighted_bias, group, rate) %>%
+    spread(group, rate) %>%
+    mutate(odds_r = black/white) -> raw_or
+  
+  ggplot(plot.dat2, aes(x=weighted_bias, y=est)) +
+    geom_line() +
+    geom_ribbon(aes(ymin=lower, ymax=upper), alpha=.15) +
+    theme_classic() +
+    facet_wrap(~metric, scales='free') -> plot
+  
+  plot_path <- '/Users/travis/Documents/gits/educational_disparities/figs/individual_models/mw_2013uclaexcl_pca/bias_OR.jpeg'
+  
+  ggsave(filename=plot_path, plot)
+
+  
+  data.frame(est=mean(p_cons[14,]),
+             lower=quantile(p_cons[14,], .025),
+             upper=quantile(p_cons[14,], .975),
+             metric=i) %>%
+    rbind(plot.dat3) %>%
+    filter(!is.na(est)) %>%
+    mutate(metric=reorder(metric, est)) -> plot.dat3
+  
+  ggplot(plot.dat3, aes(x=metric, y=est)) +
+    geom_point() +
+    geom_errorbar(aes(ymin=lower, ymax=upper)) +
+    theme_classic() +
+    coord_flip() +
+    geom_hline(yintercept=0) -> plot
+  
+  plot_path <- '/Users/travis/Documents/gits/educational_disparities/figs/individual_models/mw_2013uclaexcl_pca/bias_OR_slope.jpeg'
+  
+  ggsave(filename=plot_path, plot)
+}
+
+
+#weighted warmth w/exclusions & explicit difference
+plot.dat <- data.frame(group = NA, weighted_explicit_diff = NA, est = NA, lower = NA, upper=NA, metric = NA)
+plot.dat2 <- data.frame(weighted_explicit_diff = NA, est = NA, lower=NA, upper=NA, metric=NA)
+plot.dat3 <- data.frame(est=NA, lower=NA, upper=NA, metric=NA)
+
+for (i in metrics){
+  pth <- paste(base_path, i, '/', sep='')
+  
+  files <- list.files(pth)
+  j <- 1
+  posterior_combo <- array(0, dim=c(24,4000,length(files)))
+  raw_rate <- data.frame(county_id=NA, nschools=NA, weighted_explicit_diff=NA, group=NA, nincidents=NA, nstudents=NA, rate=NA)
+  if(length(files)>1){
+    for (k in files){
+      #print(i)
+      load(paste(pth,k,sep=''))
+      df <- as.matrix(m)
+      m$data %>% 
+        select(county_id, COMBOKEY) %>% 
+        distinct() %>% 
+        group_by(county_id) %>% 
+        summarise(nschools=n()) -> n_schools
+      m$data %>%
+        group_by(county_id, group) %>%
+        summarise(nincidents=sum(number), nstudents=sum(total_number)) %>%
+        mutate(rate=nincidents/nstudents) -> rate
+      n_schools %>%
+        left_join(m$data[, c('county_id', 'weighted_explicit_diff')]) %>%
+        distinct() %>%
+        left_join(rate) %>%
+        rbind(raw_rate) %>%
+        filter(!is.na(county_id)) -> raw_rate
+      posterior_combo[,,j] <- t(df[,c(1:24)])
+      j <- j+1
+    }
+    p_cons <- parallelMCMCcombine::consensusMCcov(posterior_combo, shuff = T)
+    
+    for (p in 1:24){
+      m$stanfit@sim$samples[[1]][[p]] <- p_cons[p,1:1000]
+    }
+    for (p in 1:24){
+      m$stanfit@sim$samples[[2]][[p]] <- p_cons[p,1001:2000]
+    }
+    for (p in 1:24){
+      m$stanfit@sim$samples[[3]][[p]] <- p_cons[p,2001:3000]
+    }
+    for (p in 1:24){
+      m$stanfit@sim$samples[[4]][[p]] <- p_cons[p,3001:4000]
+    }
+  } else{
+    load(paste(pth,files,sep=''))
+    df <- as.matrix(m)
+    m$data %>% 
+      select(county_id, COMBOKEY) %>% 
+      distinct() %>% 
+      group_by(county_id) %>% 
+      summarise(nschools=n()) -> n_schools
+    # m$data %>% 
+    #   group_by(county_id, group) %>% 
+    #   summarise(nincidents=sum(number), nstudents=sum(total_number)) %>% 
+    #   mutate(rate=nincidents/nstudents) -> rate
+    # n_schools %>% 
+    #   left_join(m$data[, c('county_id', 'weighted_warmth')]) %>%
+    #   distinct() %>%
+    #   left_join(rate) %>%
+    #   rbind(raw_rate) %>%
+    #   filter(!is.na(county_id)) -> raw_rate
+    
+  }
+  
+  newdat <- expand.grid(group = rownames(table(m$data$group)),
+                        total_pop=0,
+                        ses=0,
+                        unemp_rate=0,
+                        med_income=0,
+                        poverty_rate=0,
+                        col_grads=0,
+                        white_prop=0,
+                        black_prop=0,
+                        b.w.ratio=0,
+                        weighted_bias = 0,
+                        mobility=0,
+                        crime_rate=0,
+                        dissim=0,
+                        density=0,
+                        weighted_explicit_diff = seq(-2,2,.25),
+                        number=0,
+                        total_number=1)
+  y_hat <- rstanarm::posterior_linpred(m, newdata = newdat, re.form=~0, transform=T)
+  
+  cbind(newdat, t(y_hat)) %>% 
+    mutate(index=row_number()) %>%
+    gather(sample, value, `1`:`4000`) -> posterior_distribution
+  
+  posterior_distribution %>%
+    select(group, weighted_explicit_diff, sample, value, index) %>%
+    group_by(group, weighted_explicit_diff) %>%
+    summarise(est = mean(value),
+              lower = quantile(value, .025),
+              upper = quantile(value, .975)) %>%
+    ungroup() %>%
+    mutate(metric=i) %>%
+    rbind(plot.dat) %>% 
+    filter(!is.na(group)) -> plot.dat
+  
+  ggplot(plot.dat, aes(x=weighted_explicit_diff, y=est, group=group)) +
+    geom_line(aes(color=group)) +
+    geom_ribbon(aes(ymin=lower, ymax=upper), alpha=.15) +
+    theme_classic() +
+    facet_wrap(~metric, scales = 'free') -> plot
+  
+  plot_path <- '/Users/travis/Documents/gits/educational_disparities/figs/individual_models/mw_2013uclaexcl_pca/warmth.jpeg'
+  
+  ggsave(filename=plot_path, plot)
+  
+  # ggplot(plot.dat, aes(x=weighted_warmth, y=est, group=group)) +
+  #   geom_point(data=raw_rate, aes(x=weighted_warmth, y=rate, group=group, color=group, size=nschools), alpha=.05)+
+  #   geom_line(aes(color=group)) +
+  #   geom_ribbon(aes(ymin=lower, ymax=upper), alpha=.15) +
+  #   theme_classic() +
+  #   ylim(0,quantile(raw_rate$rate, .9)) +
+  #   facet_wrap(~metric, scales = 'free') -> plot
+  # 
+  # plot_path <- '/Users/travis/Documents/gits/educational_disparities/figs/individual_models/mw_2013uclaexcl_pca/warmth_wdata.jpeg'
+  # 
+  # ggsave(filename=plot_path, plot)
+  
+  posterior_distribution %>%
+    select(group, weighted_explicit_diff, sample, value) %>%
+    spread(group, value) %>%
+    mutate(ratio = black/white) %>%
+    select(weighted_explicit_diff, sample, ratio) %>%
+    group_by(weighted_explicit_diff) %>%
+    summarise(est = mean(ratio),
+              lower = quantile(ratio, .025),
+              upper = quantile(ratio, .975)) %>%
+    ungroup() %>%
+    mutate(metric=i) %>%
+    rbind(plot.dat2) %>% 
+    filter(!is.na(weighted_explicit_diff)) -> plot.dat2
+  
+  raw_rate %>%
+    select(county_id, nschools, weighted_explicit_diff, group, rate) %>%
+    spread(group, rate) %>%
+    mutate(odds_r = black/white) -> raw_or
+  
+  ggplot(plot.dat2, aes(x=weighted_explicit_diff, y=est)) +
+    geom_line() +
+    geom_ribbon(aes(ymin=lower, ymax=upper), alpha=.15) +
+    theme_classic() +
+    facet_wrap(~metric, scales='free') -> plot
+  
+  plot_path <- '/Users/travis/Documents/gits/educational_disparities/figs/individual_models/mw_2013uclaexcl_pca/warmth_OR.jpeg'
+  
+  ggsave(filename=plot_path, plot)
+  
+  # raw_or$mercer_county <- FALSE
+  # raw_or$mercer_county[which(raw_or$county_id=='NJ-021')] <- TRUE
+  # 
+  # # ggplot(plot.dat2, aes(x=weighted_bias, y=est)) +
+  # geom_point(data=raw_or, aes(x=weighted_bias, y=odds_r, size=nschools,
+  #                             color=mercer_county, alpha=mercer_county)) +
+    #   geom_line() +
+    #   geom_ribbon(aes(ymin=lower, ymax=upper), alpha=.15) +
+    #   theme_classic() +
+    #   scale_color_manual(values=c('grey', 'darkorange'), guide=F) + 
+    #   scale_alpha_manual(values=c(.1, 1), guide=F) + 
+    #   ylim(0,quantile(raw_or$odds_r, na.rm=T, .98)) +
+    #   xlab('Standardized Implicit Bias') +
+    #   ylab('Relative Risk Ratio') +
+    #   geom_hline(yintercept=1) +
+    #   theme(strip.text = element_text(size=14),
+    #         axis.title = element_text(face='bold', size=16),
+  #         axis.text = element_text(size=14),
+  #         legend.title = element_text(size=14),
+  #         legend.text = element_text(size=14)) -> plot
+  # ggsave('/Users/travis/Desktop/implicit_rr.jpeg', plot)
+  
+  # ggplot(plot.dat2, aes(x=weighted_explicit_diff, y=est)) +
+  #   geom_point(data=raw_or, aes(x=weighted_explicit_diff, y=odds_r, size=nschools,
+  #                               color=mercer_county, alpha=mercer_county)) +
+  #   geom_line() +
+  #   geom_ribbon(aes(ymin=lower, ymax=upper), alpha=.15) +
+  #   theme_classic() +
+  #   scale_color_manual(values=c('grey', 'darkorange'), guide=F) + 
+  #   scale_alpha_manual(values=c(.1, 1), guide=F) +
+  #   ylim(0,7) +
+  #   xlab('Standardized Explicit Bias') +
+  #   ylab('Relative Risk Ratio') +
+  #   geom_hline(yintercept=1) +
+  #   theme(strip.text = element_text(size=14),
+  #         axis.title = element_text(face='bold', size=16),
+  #         axis.text = element_text(size=14),
+  #         legend.title = element_text(size=14),
+  #         legend.text = element_text(size=14)) -> plot
+  # ggsave('/Users/travis/Desktop/explicit_rr.jpeg', plot)
+  # 
+  # plot_path <- '/Users/travis/Documents/gits/educational_disparities/figs/individual_models/mw_uclaexcl/warmth_OR_wdata.jpeg'
+  # 
+  # ggsave(filename=plot_path, plot)  
+  
+  data.frame(est=mean(p_cons[15,]),
+             lower=quantile(p_cons[15,], .025),
+             upper=quantile(p_cons[15,], .975),
+             metric=i) %>%
+    rbind(plot.dat3) %>%
+    filter(!is.na(est)) %>%
+    mutate(metric=reorder(metric, est)) -> plot.dat3
+  
+  ggplot(plot.dat3, aes(x=metric, y=est)) +
+    geom_point() +
+    geom_errorbar(aes(ymin=lower, ymax=upper)) +
+    theme_classic() +
+    coord_flip() +
+    geom_hline(yintercept=0) -> plot
+  
+  plot_path <- '/Users/travis/Documents/gits/educational_disparities/figs/individual_models/mw_2013uclaexcl_pca/warmth_OR_slope.jpeg'
+  
+  ggsave(filename=plot_path, plot)
+}
+
+
+
+
+
+
+
+
+
+##########################################
+
+
+
 base_path <- '/Users/travis/Documents/gits/educational_disparities/cluster/output/metrics_raw/'
 plot.dat <- data.frame(group = NA, bias = NA, est = NA, lower = NA, upper=NA, metric = NA)
 plot.dat2 <- data.frame(bias = NA, est = NA, lower=NA, upper=NA, metric=NA)
